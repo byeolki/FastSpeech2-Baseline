c8b5498580f9236021416160497856acd1afe5ee
byeolki <byeolki7007@gmail.com>
2026-01-18 06:59:22 +0000

    feat: apply aria2 to download script(prepare.sh)

--- recipes/ljspeech/prepare.sh
+++ recipes/ljspeech/prepare.sh
@@ -5,7 +5,7 @@ DATASET_DIR="data/raw/ljspeech"
 mkdir -p $DATASET_DIR
 
 echo "Downloading LJSpeech dataset..."
-wget -P $DATASET_DIR https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2
+aria2c -x 16 -s 16 -k 1M https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2 -d $DATASET_DIR
 
 echo "Extracting dataset..."
 tar -xjf $DATASET_DIR/LJSpeech-1.1.tar.bz2 -C $DATASET_DIR
